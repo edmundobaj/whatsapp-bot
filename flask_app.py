fad79ee6bbd3640f0889a352480159d354b4ad25
edmundobaj <edmundobaj@gmail.com>
2022-01-20 10:56:43 -0400

    Correçao da declaração do atributo session

--- flask_app.py
+++ flask_app.py
@@ -6,7 +6,7 @@ from twilio.twiml.messaging_response import MessagingResponse
 app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-session = Session(app)
+Session(app)
 
 
 @app.route('/bot', methods=['POST'])
