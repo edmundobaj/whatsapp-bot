d7918fe913c1c961d4fa3c06566b521ff00aaa97
edmundobaj <edmundobaj@gmail.com>
2022-01-20 10:49:06 -0400

    Correção de imports

--- flask_app.py
+++ flask_app.py
@@ -6,7 +6,7 @@ from twilio.twiml.messaging_response import MessagingResponse
 app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+session = Session(app)
 
 
 @app.route('/bot', methods=['POST'])
